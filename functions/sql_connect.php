cf0ceac15a3e3756b73c282d069ce9891eaa30be
Pancakes13 <neil.llenes@gmail.com>
2017-12-17 21:28:53 +0800

    Changed to the new DB server(Old one wasn't working)

--- functions/sql_connect.php
+++ functions/sql_connect.php
@@ -1,8 +1,8 @@
 <?php
-	$server_address = "sql12.freemysqlhosting.net"; 
-	$username = "sql12208242";
-	$password = "2gYimpae7m";
-	$database_name = "sql12208242";
+	$server_address = "den1.mysql2.gear.host"; 
+	$username = "unwind";
+	$password = "Gb6H8udjsi-?";
+	$database_name = "unwind";
 	$port = "3306";
 
 	$mysqli = new mysqli($server_address, $username, $password, $database_name, $port);
