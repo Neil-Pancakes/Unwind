e697652400833d0ea830a0047cde913749a6553d
Pancakes13 <neil.llenes@gmail.com>
2017-12-27 12:53:58 +0800

    Created an Imprints page for flaticon license
    This page is created for legal reasons

---
+++ views/imprints.php
@@ -0,0 +1,21 @@
+<?php
+require ("../header.php");
+include '../sidebar.php';
+?>
+ <!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    
+<div>Icons made by <a href="https://www.flaticon.com/authors/vectors-market" title="Vectors Market">Vectors Market</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+<div>Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a> is licensed by <a href="http://creativecommons.org/licenses/by/3.0/" title="Creative Commons BY 3.0" target="_blank">CC 3.0 BY</a></div>
+
+</div>
+
+<?php
+include '../footer.php';
+include '../control_sidebar.php';
+?>
+<!-- End of div wrapper-->
+</div>
+<!-- End of body-->
+</body>
+
