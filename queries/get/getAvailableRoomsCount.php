962d2cf0cf84b2238a02cc6807ef6ce80518ffd8
Pancakes13 <neil.llenes@gmail.com>
2018-01-04 00:51:50 +0800

    Created a tester for getting available room count

--- queries/get/getAvailableRoomsCount.php
+++ queries/get/getAvailableRoomsCount.php
@@ -15,36 +15,40 @@ AND `r`.`room_type_id` = $val
 LIMIT $qty");
 */
 
-$checkIn = $request['check_in_date'];
-$checkOut = $request['check_out_date'];
-$type = $request['roomType'];
-$qty = $request['quantity'];
+$checkIn = '2018-01-05';
+$checkOut = '2018-01-31';
 
 $result = $mysqli->query("SELECT `r`.`room_id`, `r`.`room_number`, `rr`.`checkin_date`, `rr`.`checkout_date`
-FROM `room` `r`
-INNER JOIN `room_reserved` `res`
-ON `r`.`room_id` = `res`.`room_id` AND `r`.`room_type_id`=$type
-INNER JOIN `reservation_request` `rr`
-ON `rr`.`reservation_request_id` = `res`.`reservation_request_id`
-AND ((`rr`.`checkin_date` <= '$checkOut' AND `rr`.`checkout_date` > '$checkIn')
-OR (`rr`.`checkout_date` > '$checkOut' AND `rr`.`checkin_date` <= '$checkIn'))");
+    FROM `room` `r`
+    INNER JOIN `room_reserved` `res`
+    ON `r`.`room_id` = `res`.`room_id`
+    INNER JOIN `reservation_request` `rr`
+    ON `rr`.`reservation_request_id` = `res`.`reservation_request_id`
+    AND ((`rr`.`checkin_date` <= '$checkOut' AND `rr`.`checkout_date` > '$checkIn')
+    OR (`rr`.`checkout_date` > '$checkOut' AND `rr`.`checkin_date` <= '$checkIn'))");
 
-$qry = "SELECT COUNT(*) AS 'type_count', `t`.`room_type_id`, `t`.`name`
-FROM `room` `r`
-INNER JOIN `room_type` `t`
-ON `r`.`room_type_id` = `t`.`room_type_id`
-AND `r`.`room_status` = 'Available'
-AND `r`.`room_type_id` = $type";
+$qry = "SELECT COUNT(*) AS 'type_count', `t`.`room_type_id`, `t`.`name`, `t`.`price`, `t`.`description`
+    FROM `room` `r`
+    INNER JOIN `room_type` `t`
+    ON `r`.`room_type_id` = `t`.`room_type_id`";
 while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
-    $qry .= "AND NOT `r`.`room_id` = ".$rs['room_id']."";
+    $qry .= " AND NOT `r`.`room_id` = ".$rs['room_id']."";
 }
-$qry .="LIMIT $qty";
+$qry .= " GROUP BY `t`.`room_type_id`";
 
-$rs = $qry->fetch_array(MYSQLI_ASSOC);
+$result2 = $mysqli->query($qry);
+
 $outp = "";
-$outp .= '{"RoomTypeId":"'  . $rs["room_type_id"] . '",';
-$outp .= '"Name":"'  . $rs["name"] . '",';
-$outp .= '"TypeCount":"'   . $rs["type_count"]        . '"}';
+while($rs2 = $result2->fetch_array(MYSQLI_ASSOC)) {
+    if ($outp != "") {
+        $outp .= ",";
+    }
+    $outp .= '{"RoomTypeId":"'  . $rs2["room_type_id"] . '",';
+    $outp .= '"Name":"'  . $rs2["name"] . '",';
+    $outp .= '"Name":"'  . $rs2["name"] . '",';
+    $outp .= '"Name":"'  . $rs2["name"] . '",';
+    $outp .= '"TypeCount":"'   . $rs2["type_count"]        . '"}';
+}
 
 $outp ='{"records":['.$outp.']}';
 $mysqli->close();
