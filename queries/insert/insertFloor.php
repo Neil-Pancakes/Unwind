5dd6111822390e09a856381e8804af307f26d8b1
Pedro.Pau <rpaulojade@gmail.com>
2018-01-25 16:45:03 +0800

    Fixed Something Weird

--- queries/insert/insertFloor.php
+++ queries/insert/insertFloor.php
@@ -4,14 +4,8 @@
   $request = json_decode($postdata, true);
   
   if(count($request>0)){
-    $floor=$request['floor'];
-    $query = "INSERT INTO `floor` (`floor_number`) VALUES ('$floor');";
-    $number = $request['floor_number'];
-    
-    $query = "INSERT INTO `floor` 
-    (`floor_number`) 
-    VALUES 
-    ($number);";
+    $number=$request['floor'];
+    $query = "INSERT INTO `floor` (`floor_number`) VALUES ('$number');";
     $result = mysqli_query($mysqli, $query);
   }else{
       echo "error";
