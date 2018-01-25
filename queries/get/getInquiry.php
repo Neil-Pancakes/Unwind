f7948786741963a2d7568a7ccecb39925e4af305
Pancakes13 <neil.llenes@gmail.com>
2018-01-25 16:17:31 +0800

    Fixed Colina's queries

--- queries/get/getInquiry.php
+++ queries/get/getInquiry.php
@@ -5,18 +5,12 @@ require("../../functions/sql_connect.php");
 session_start();
 
 //$emp_id = $_SESSION['user_id'];
-$result = $mysqli->query("SELECT `f`.`inquiry_id`, `f`.`message`, `f`.`	user_id`, 
+$result = $mysqli->query("SELECT `i`.`inquiry_id`, `i`.`message`, `u`.`user_id`, `i`.`inquiry_timestamp`,
 CONCAT(`u`.`first_Name`,' ', `u`.`middle_initial`, ' ', `u`.`last_name`) AS `name`
-FROM `inquiry` `f`
-INNER JOIN `check_in` `c`
-ON `f`.`check_in_id` = `c`.`check_in_id`
-INNER JOIN `reservation` `r`
-ON `r`.`reservation_id` = `c`.`reservation_id`
-INNER JOIN `reservation_request` `rr`
-ON `rr`.`reservation_request_id` = `r`.`reservation_request_id`
+FROM `inquiry` `i`
 INNER JOIN `user_account` `u`
-ON `rr`.`user_id` = `u`.`user_id`
-ORDER BY `c`.`check_in_end`");
+ON `i`.`user_id` = `u`.`user_id`
+ORDER BY `i`.`inquiry_timestamp` DESC");
 
 $outp = "";
 while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
@@ -24,8 +18,10 @@ while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
         $outp .= ",";
     }
     $outp .= '{"InquiryId":"'  . $rs["inquiry_id"] . '",';
-    $outp .= '"message":"'  . $rs["message"] . '",';
-    $outp .= '"user_id":"'   . $rs["user_id"]        . '"}';
+    $outp .= '"Message":"'  . $rs["message"] . '",';
+    $outp .= '"Timestamp":"'  . $rs["inquiry_timestamp"] . '",';
+    $outp .= '"Name":"'  . $rs["name"] . '",';
+    $outp .= '"UserId":"'   . $rs["user_id"]        . '"}';
 }
 $outp ='{"records":['.$outp.']}';
 $mysqli->close();
